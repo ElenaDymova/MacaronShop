c32be2257b365ed9a922046c10ca7eb5157c4086
Elena <ebe2017@yandex.ru>
2024-10-15 18:40:58 +0300

    the beginning of the project

---
+++ src/App.js
@@ -0,0 +1,92 @@
+
+
+function App() {
+  return (
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={50} height={50} src="/img/logo.svg"/>
+          <div>
+            <h3 className="text-uppercase">MACARON SHOP</h3>
+            <p className="opacity-5">Shop for desserts made from natural ingredients</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/cart.svg"/>
+            <span>112$</span>
+          </li>
+          <li>
+          <img width={18} height={18} src="/img/user.svg"/>
+          </li>
+        </ul>
+      </header>
+      <div className="content p-40">
+        <h1 className="mb-40">All desserts</h1>
+
+        <div className="d-flex">
+          <div className="card">
+            <img width={133} height={112} src="/img/macarons/macaron1.png" alt="Macarons"/>
+            <h5>Сезонный вкус: молочный шоколад, отборные свежие бананы</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Price:</span>
+                <b>12$</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
+              </button>
+            </div>
+          </div>
+
+
+          <div className="card">
+            <img width={133} height={112} src="/img/macarons/macaron2.png" alt="Macarons"/>
+            <h5>Сезонный вкус: молочный шоколад, отборные свежие бананы</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Price:</span>
+                <b>12$</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img width={133} height={112} src="/img/macarons/macaron3.png" alt="Macarons"/>
+            <h5>Сезонный вкус: молочный шоколад, отборные свежие бананы</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Price:</span>
+                <b>12$</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img width={133} height={112} src="/img/macarons/macaron4.png" alt="Macarons"/>
+            <h5>Сезонный вкус: молочный шоколад, отборные свежие бананы</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Price:</span>
+                <b>12$</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
+              </button>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;

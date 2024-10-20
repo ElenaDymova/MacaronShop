baf8e3a7f979f5bb93c072f59caacea7b5552d0d
Elena <ebe2017@yandex.ru>
2024-10-20 13:35:20 +0300

    work with api and cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
@@ -6,37 +7,62 @@ import Drawer from './components/Drawer';
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-    fetch('https://66c4b6a6b026f3cc6cf06c1e.mockapi.io/items').then(res => {
-      return res.json();
-    }).then(json => {
-      setItems(json);
+    axios.get('https://66c4b6a6b026f3cc6cf06c1e.mockapi.io/items').then(res => {
+      setItems(res.data)
+    });
+    axios.get('https://66c4b6a6b026f3cc6cf06c1e.mockapi.io/cart').then(res => {
+      setCartItems(res.data)
     });
   }, [])
 
   const onAddToCart = (obj) => {
+    axios.post('https://66c4b6a6b026f3cc6cf06c1e.mockapi.io/cart', obj);
     setCartItems(prev => [...prev, obj]);
+  }
+
+  const onRemoveItem = (id) => {
+    axios.delete(`https://66c4b6a6b026f3cc6cf06c1e.mockapi.io/cart/${id}`);
+    setCartItems(prev => prev.filter(item => item.id !== id));
+  }
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
   }
 
   return (
     <div className="wrapper clear">
-      {cartOpened ? <Drawer items={cartItems} onClose = {() => setCartOpened(false)}/> : null}
+      {cartOpened ? <Drawer onRemove={onRemoveItem} items={cartItems} onClose = {() => setCartOpened(false)}/> : null}
       <Header onClickCart = {() => setCartOpened(true)}/>
 
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>All desserts</h1>
+          <h1>{searchValue ? `search by: "${searchValue}"` : 'All desserts'}</h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search"/>
-            <input placeholder="Поиск..."/>
+            {searchValue && (
+              <img 
+                onClick={() => setSearchValue('')}
+                className="removeBtn clear cu-p" 
+                src="/img/btn-remove.svg" 
+                alt="Clear"
+              />)}
+            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..."/>
           </div>
         </div>
 
         <div className="d-flex flex-wrap">
-          {items.map((obj) => (
+          {items
+            .filter(item => 
+              item.name.toLowerCase().includes(searchValue.toLowerCase()) || 
+              item.description.toLowerCase().includes(searchValue.toLowerCase()) 
+            )
+            .map((obj, index) => (
               <Card 
+                key={index}
                 title={obj.name}
                 description={obj.description}
                 price={obj.price} 
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,39 +1,52 @@
-function Drawer({ onClose, items = [] }) {
+function Drawer({ onClose, onRemove, items = [] }) {
     return (
         <div className="overlay">
             <div className="drawer">
                 <h2 className="d-flex justify-between mb-30">Shopping cart 
-                <img onClick={onClose} className="removeBtn cu-p" src="/img/btn-remove.svg" alt="remove"/>
+                    <img onClick={onClose} className="removeBtn cu-p" src="/img/btn-remove.svg" alt="remove"/>
                 </h2>
 
-                <div className="items">
-                    {items.map((obj) => (
-                        <div className="cartItem d-flex align-center mb-20">
-                            <div style={{ backgroundImage: `url(${obj.img})` }} className="cartItemImg"></div>
-                            <div className="mr-20">
-                            <p className="mb-5">{obj.name}</p>
-                            <b>{obj.price}</b>
-                            </div>
-                            <img className="removeBtn" src="/img/btn-remove.svg" alt="remove"/>
+                {
+                    items.length > 0 ? 
+                    <div>
+                        <div className="items">
+                            {items.map((obj) => (
+                                <div className="cartItem d-flex align-center mb-20">
+                                    <div style={{ backgroundImage: `url(${obj.img})` }} className="cartItemImg"></div>
+                                    <div className="mr-20">
+                                    <p className="mb-5">{obj.name}</p>
+                                    <b>{obj.price}</b>
+                                    </div>
+                                    <img onClick={() => onRemove(obj.id)} className="removeBtn" src="/img/btn-remove.svg" alt="remove"/>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-
-                <div className="cartTotalBlock">
-                <ul>
-                    <li>
-                    <span>Итого:</span>
-                    <div></div>
-                    <b>200$</b>
-                    </li>
-                    <li>
-                    <span>Tax 5%:</span>
-                    <div></div>
-                    <b>10$</b>
-                    </li>
-                </ul>
-                <button className="pinkButton">Place an order <img src="/img/arrow.svg" alt="arrow"/> </button>
-                </div>
+                        <div className="cartTotalBlock">
+                            <ul>
+                                <li>
+                                <span>Итого:</span>
+                                <div></div>
+                                <b>200$</b>
+                                </li>
+                                <li>
+                                <span>Tax 5%:</span>
+                                <div></div>
+                                <b>10$</b>
+                                </li>
+                            </ul>
+                            <button className="pinkButton">Place an order <img src="/img/arrow.svg" alt="arrow"/> </button>
+                        </div>
+                    </div> :
+                    <div className="cartEmpty d-flex align-center justify-center flex-column flex">
+                        <img className="mb-20" width="120px" height="120px" src="/img/empty-cart.svg" alt="Empty cart"/>
+                        <h2>Empty shopping cart</h2>
+                        <p className="opacity-6">Add at least 1 dessert to place an order</p>
+                        <button onClick={onClose} className="pinkButton">
+                            <img src="/img/arrow.svg" alt="Arrow"/>
+                            Go back
+                        </button>
+                    </div>
+                }
 
             </div>
         </div>
